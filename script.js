347080637a7b0f7751eef2d3625e8f183b68104a
이상현 <aqjquqhiqjq@gmail.com>
2023-05-06 15:42:58 +0900

    리터럴

--- script.js
+++ script.js
@@ -6,3 +6,10 @@ var javascript = 10; // 변수 javascript와 JavaScript는 서로 다른 두 개
 var JavaScript = 20;
 var Script = 30; // 변수의 선언은 var 키워드로만 할 수 있으면 Var는 동작하지 않음.
 
+
+12 // 숫자 리터럴
+"javascript" // 문자열 리터럴
+"안녕하세요" //문자열 리터럴
+true // 불리언 리터럴
+
+
